fe13b77c367c3cdcfa677eb9cfac86d73ac554ee
Ervin Ungvari <ungvari.ervin@yahoo.com>
2023-05-29 20:42:34 +0300

    Working on the given tasks

---
+++ Functions/app.js
@@ -0,0 +1,13 @@
+// 1) Write a function that takes an array of numbers as input and returns the largest number in the array.
+
+const largestNum = (...numArr) => {
+    return Math.max(...numArr);
+};
+
+console.log(largestNum(1, 4, 26, 12, 337, 9, 45, 11, 3));
+
+// 2) Create a simple calculator that can perform basic arithmetic operations (+, -, *, /) on two numbers.
+
+const calculator = (param1, param2, operator) => {
+    
+};
